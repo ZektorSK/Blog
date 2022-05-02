7c52743e05021622dd7553d9bcc3f9038d67c448
zektor <dankomajchrak423@gmail.com>
2022-05-02 23:59:56 +0200

    final designing

--- client/src/components/AllBlogs.vue
+++ client/src/components/AllBlogs.vue
@@ -1,9 +1,10 @@
 <template>
-    <article class="flex flex-col justify-center self-center">
+    <article class="flex flex-col justify-center self-center p-8">
         <ul class="flex flex-col justify-center self-center text-center max-w-[950px]">
-            <li class="p-4">
+            <li class="p-4 hover:text-neutral-800">
                 <div :class="TCSSpostdiv">
-                    <a :class="TCSSpostanchor">Blog post #1</a>
+                    <p :class="TCSSpostparagraph" class="text-left underline decoration-green-400 italic">2.5.2022</p>
+                    <h3 :class="TCSSpostanchor">Blog post #1</h3>
                     <p :class="TCSSpostparagraph">How I made a single mistake. Another story of mine...</p>
                 </div>
             </li>
@@ -15,9 +16,9 @@
 export default{
     data(){
         return{
-            TCSSpostdiv: "hover:shadow-lg hover:bg-green-300 border-4 border-green-300 hover:cursor-pointer p-5",
-            TCSSpostanchor: "color-neutral-900 font-bold font-serif text-lg",
-            TCSSpostparagraph: "font-mono font-light text-sm italic"
+            TCSSpostdiv: "hover:shadow-lg hover:bg-green-400 hover:text-neutral-800 border-4 border-green-400 hover:cursor-pointer p-5 bg-neutral-800 rounded-lg",
+            TCSSpostanchor: "text-neutral-100 font-bold font-serif text-xl pb-4 pt-2",
+            TCSSpostparagraph: "font-mono font-light text-sm text-neutral-200"
         }
     }
 }
--- client/src/components/CommentVue.vue
+++ client/src/components/CommentVue.vue
@@ -1,18 +1,35 @@
 <template>
-    <footer class="flex justify-center self-center fixed bottom-0 pb-9 flex-col border-t-4 border-green-600 ">
-        <div class="flex justify-center self-center flex-col">
-            <h3 class="text-xl font-serif text-center">Comments</h3>
-            <button type="submit" :class="TCSSbutton" v-show="!showinput" @click="showinput=true">Post</button>
-            <button type="submit" :class="TCSSbutton" v-show="showinput" @click="showinput=false">Hide</button>
-            <form class="pt-2" v-show="showinput">
+    <footer class="flex justify-center justify-self-center pb-4 fixed flex-col border-t-4 w-[600px] left-1/2 ml-[-300px] bottom-0" style="border-color: rgb(65,184,131);">
+        <div class="flex justify-center justify-self-center flex-col">
+
+            <h3 class="text-xl font-serif text-center text-slate-100 pt-2">Comments</h3>
+            <span class="flex justify-left">
+                <button type="submit" :class="TCSSbutton" v-show="!showinput" @click="showinput=true">Post a comment!</button>
+            </span>
+
+            <form class="p-2 flex justify-center justify-self-center flex-col border-2 border-neutral-800" v-show="showinput">
                 <label for="name" :class="TCSSlabel">Name: </label>
-                <input type="text" name="name" :class="TCSStextinput" required>
-                <label for="text" :class="TCSSlabel">Text:</label>
-                <textarea placeholder="type something..." class="border-2" name="text" required></textarea>
-                <button type="submit" :class="TCSSbutton">Submit</button>
+                <input type="text" name="name" :class="TCSStextinput" style="border-color: rgb(65,184,131);" required>
+                <label for="text" :class="TCSSlabel">Message:</label>
+                <textarea placeholder="type something..." class="border-2 bg-neutral-800 text-slate-100" style="border-color: rgb(65,184,131);" name="text" required></textarea>
+                <span class="flex gap-4">  
+                    <button type="submit" :class="TCSSbutton">Submit</button>
+                    <button type="submit" :class="TCSSbutton" v-show="showinput" @click="showinput=false">Hide</button>
+                </span>
             </form>
 
-
+            <ul>
+                <li :class="TCSScommentDiv">            
+                    <h4 :class="TCSScommentAuthor">Daniel</h4>
+                    <pre :class="TCSScommentText">Damn, nice post bro</pre>
+                    <p :class="TCSScommentTime">2.5.2022 23:41</p>
+                </li>
+                <li :class="TCSScommentDiv">            
+                    <h4 :class="TCSScommentAuthor">Anna</h4>
+                    <pre :class="TCSScommentText">Zekky wukky I vuw u UwU</pre>
+                    <p :class="TCSScommentTime">2.5.2022 23:52</p>
+                </li>
+            </ul>
             
         </div>
     </footer>
@@ -22,11 +39,21 @@
 export default{
     data(){
         return{
-            showinput: true,
-            TCSStextinput: "border-2 h-[25px] w-[300px]",
-            TCSSlabel: "text-base",
-            TCSSbutton: "border-4 border-green-300 hover:bg-green-300 block p-1 hover:shadow-lg"
+            showinput: false,
+            TCSStextinput: "border-2 h-[25px] w-[150px] bg-neutral-800 text-slate-100",
+            TCSSlabel: "text-base text-slate-100",
+            TCSSbutton: "block border-4 border-green-400 hover:bg-green-400 block p-2 hover:shadow-lg text-slate-100 bg-neutral-800 rounded-lg mt-2",
+            TCSScommentTime: "font-mono text-slate-400 text-right text-sm p-2 underline decoration-slate-600",
+            TCSScommentAuthor: "font-serif text-slate-100 text-xl ml-4",
+            TCSScommentText: "font-mono text-slate-200 text-base text-center",
+            TCSScommentDiv: "border-2 border-neutral-800 mt-6"
         }
     }
 }
 </script>
+
+<style scoped>
+textarea:focus, input:focus{
+    outline: none;
+}
+</style>
