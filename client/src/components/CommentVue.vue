559fcfdbdc35a226b83c954f615470e9e23e6c4f
zektor <dankomajchrak423@gmail.com>
2022-05-03 20:32:38 +0200

    finishing

--- client/src/components/CommentVue.vue
+++ client/src/components/CommentVue.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="flex justify-center justify-self-center pb-4 fixed flex-col border-t-4 w-[600px] left-1/2 ml-[-300px] bottom-0" style="border-color: rgb(65,184,131);">
+    <footer class="flex justify-center justify-self-center pb-4 flex-col border-t-4 max-w-[600px]" style="border-color: rgb(65,184,131);">
         <div class="flex justify-center justify-self-center flex-col">
 
             <h3 class="text-xl font-serif text-center text-slate-100 pt-2">Comments</h3>
@@ -9,25 +9,20 @@
 
             <form class="p-2 flex justify-center justify-self-center flex-col border-2 border-neutral-800" v-show="showinput">
                 <label for="name" :class="TCSSlabel">Name: </label>
-                <input type="text" name="name" :class="TCSStextinput" style="border-color: rgb(65,184,131);" required>
+                <input type="text" name="name" :class="TCSStextinput" style="border-color: rgb(65,184,131);" required v-model="author">
                 <label for="text" :class="TCSSlabel">Message:</label>
-                <textarea placeholder="type something..." class="border-2 bg-neutral-800 text-slate-100" style="border-color: rgb(65,184,131);" name="text" required></textarea>
+                <textarea placeholder="type something..." class="border-2 bg-neutral-800 text-slate-100" style="border-color: rgb(65,184,131);" name="text" required v-model="message"></textarea>
                 <span class="flex gap-4">  
-                    <button type="submit" :class="TCSSbutton">Submit</button>
-                    <button type="submit" :class="TCSSbutton" v-show="showinput" @click="showinput=false">Hide</button>
+                    <button type="button" :class="TCSSbutton" @click="createPost()">Submit</button>
+                    <button :class="TCSSbutton" v-show="showinput" @click="showinput=false">Hide</button>
                 </span>
             </form>
 
             <ul>
-                <li :class="TCSScommentDiv">            
-                    <h4 :class="TCSScommentAuthor">Daniel</h4>
-                    <pre :class="TCSScommentText">Damn, nice post bro</pre>
-                    <p :class="TCSScommentTime">2.5.2022 23:41</p>
-                </li>
-                <li :class="TCSScommentDiv">            
-                    <h4 :class="TCSScommentAuthor">Anna</h4>
-                    <pre :class="TCSScommentText">Zekky wukky I vuw u UwU</pre>
-                    <p :class="TCSScommentTime">2.5.2022 23:52</p>
+                <li :class="TCSScommentDiv" v-for="comment of Comments" :key="comment._id">            
+                    <h4 :class="TCSScommentAuthor">{{comment.author}}</h4>
+                    <pre :class="TCSScommentText">{{comment.text}}</pre>
+                    <p :class="TCSScommentTime">{{comment.date}}</p>
                 </li>
             </ul>
             
@@ -36,9 +31,12 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default{
     data(){
         return{
+            Comments: [],
             showinput: false,
             TCSStextinput: "border-2 h-[25px] w-[150px] bg-neutral-800 text-slate-100",
             TCSSlabel: "text-base text-slate-100",
@@ -46,7 +44,39 @@ export default{
             TCSScommentTime: "font-mono text-slate-400 text-right text-sm p-2 underline decoration-slate-600",
             TCSScommentAuthor: "font-serif text-slate-100 text-xl ml-4",
             TCSScommentText: "font-mono text-slate-200 text-base text-center",
-            TCSScommentDiv: "border-2 border-neutral-800 mt-6"
+            TCSScommentDiv: "border-2 border-neutral-800 mt-6",
+
+            author:"",
+            message:""
+        }
+    },
+    async created(){
+        try{
+            const res = await axios.get('http://localhost:3000/api/comment');
+            this.Comments = res.data;
+            this.Comments.sort((a, b) => a-b);
+
+        }catch(err){
+            console.log(`Error: ${err}`);
+        }
+    },
+    methods:{
+        async createPost(){
+            try{
+                const res = await axios.post('http://localhost:3000/api/comment', {
+                    author: this.author,
+                    text: this.message
+                });
+                this.Comments = [...this.Comments, res.data];
+                this.Comments.sort((a, b) => a-b);
+
+                this.author = "";
+                this.message = "";
+                this.showinput = false;
+            }
+            catch(err){
+                console.log(`Error: ${err}`);
+            }
         }
     }
 }
